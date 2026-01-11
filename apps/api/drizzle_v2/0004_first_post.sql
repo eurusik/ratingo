aeec6d77d72386bca3cf89e773e06b78a0054785
eurusik <john.rusakov@gmail.com>
2026-01-11 17:22:48 +0200

    fix(migration): correct constraint name for prod DB

--- apps/api/drizzle_v2/0004_first_post.sql
+++ apps/api/drizzle_v2/0004_first_post.sql
@@ -1,4 +1,4 @@
-ALTER TABLE "media_items" DROP CONSTRAINT "media_items_slug_unique";--> statement-breakpoint
+ALTER TABLE "media_items" DROP CONSTRAINT "media_items_slug_key";--> statement-breakpoint
 DROP INDEX "media_slug_idx";--> statement-breakpoint
 CREATE UNIQUE INDEX "media_type_slug_idx" ON "media_items" USING btree ("type","slug");--> statement-breakpoint
 CREATE INDEX "media_slug_idx" ON "media_items" USING btree ("slug");
